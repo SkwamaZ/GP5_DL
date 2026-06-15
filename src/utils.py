b0566b9713bd2a828c82ec6533fd5709db47368a
mgfatima <m_fatima006@mail.ru>
2026-06-15 18:35:48 +0300

    Текстовый DL

--- src/utils.py
+++ src/utils.py
@@ -20,7 +20,9 @@ def set_seed(seed):
 
 
 def get_device():
-    if torch.backends.mps.is_available():
+    if torch.cuda.is_available():
+        return torch.device("cuda")
+    if hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
         return torch.device("mps")
     return torch.device("cpu")
 
